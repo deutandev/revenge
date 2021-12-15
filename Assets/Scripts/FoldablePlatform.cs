5868fcf842e23da28c8c9c295bbc14b9ffc2e689
Andika Wirapala Fathin Azfar <dika.wfa@pm.me>
2021-12-16 06:40:17 +0700

    Add (Platform & Scene): Add foldable platform and test scene for demonstration

---
+++ Assets/Scripts/FoldablePlatform.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoldablePlatform : MonoBehaviour
+{
+    public GameObject InteractButton;
+    [SerializeField] private Animator Lever = null;
+    public List<GameObject> Platforms;
+    [SerializeField] private bool opened = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            InteractButton.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            InteractButton.SetActive(false);
+        }
+    }
+
+    public void ToggleLever()
+    {
+        if (!opened)
+        {
+            Lever.Play("handle|rotateL", 0, 0.0f);
+            
+            foreach (GameObject plat in Platforms)
+            { plat.GetComponent<Animator>().Play("Plate|Open", 0, 0.0f); }
+            
+            opened = true;
+        }
+        else if (opened)
+        {
+            Lever.Play("handle|rotateR", 0, 0.0f);
+            
+            foreach (GameObject plat in Platforms)
+            { plat.GetComponent<Animator>().Play("Plate|Close", 0, 0.0f); }
+            
+            opened = false;
+        }
+    }
+}
