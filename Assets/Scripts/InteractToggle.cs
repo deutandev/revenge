fd85e4ad8ea4f090ab84fb3c8db3b3f5594cd9b7
Andika Wirapala Fathin Azfar <dika.wfa@pm.me>
2021-12-15 18:51:46 +0700

    Setup Lever animation

---
+++ Assets/Scripts/InteractToggle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractToggle : MonoBehaviour
+{
+    public GameObject InteractButton;
+    [SerializeField] private Animator Lever = null;
+    [SerializeField] private bool opened = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            InteractButton.SetActive(true);
+            // if (!opened)
+            // {
+            //     Lever.Play("handle|rotateL", 0, 0.0f);
+            //     opened = true;
+            // }
+            // else if (opened)
+            // {
+            //     Lever.Play("handle|rotateR", 0, 0.0f);
+            //     opened = false;
+            // }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            InteractButton.SetActive(false);
+        }
+    }
+
+    public void ToggleLever()
+    {
+        if (!opened)
+        {
+            Lever.Play("handle|rotateL", 0, 0.0f);
+            opened = true;
+        }
+        else if (opened)
+        {
+            Lever.Play("handle|rotateR", 0, 0.0f);
+            opened = false;
+        }
+    }
+}
