a716dc73085d6a36f969cfcbdb06321ca4bef871
Andika Wirapala Fathin Azfar <dika.wfa@pm.me>
2021-12-15 22:59:06 +0700

    Animate foldable platform

--- Assets/Scripts/InteractToggle.cs
+++ Assets/Scripts/InteractToggle.cs
@@ -7,6 +7,8 @@ public class InteractToggle : MonoBehaviour
 {
     public GameObject InteractButton;
     [SerializeField] private Animator Lever = null;
+    [SerializeField] private Animator[] Platform;
+    public List<GameObject> Platforms;
     [SerializeField] private bool opened = false;
 
     private void OnTriggerEnter(Collider other)
@@ -40,11 +42,19 @@ public class InteractToggle : MonoBehaviour
         if (!opened)
         {
             Lever.Play("handle|rotateL", 0, 0.0f);
+            
+            foreach (GameObject plat in Platforms)
+            { plat.GetComponent<Animator>().Play("Plate|Open", 0, 0.0f); }
+            
             opened = true;
         }
         else if (opened)
         {
             Lever.Play("handle|rotateR", 0, 0.0f);
+            
+            foreach (GameObject plat in Platforms)
+            { plat.GetComponent<Animator>().Play("Plate|Close", 0, 0.0f); }
+            
             opened = false;
         }
     }
