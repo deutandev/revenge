d0119bcc319213b71a10a83e92cdccd64648ec78
RedHunter7 <adlirafif42121@gmail.com>
2021-12-15 21:20:49 +0700

    Add New Animation

---
+++ Assets/Scripts/IgnoreCollider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollider : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {   
+        
+		if (collision.gameObject.tag == "Bat" || collision.gameObject.tag == "Spider")
+		{
+			Physics.IgnoreCollision(GetComponent<MeshCollider>(), collision.gameObject.GetComponent<CapsuleCollider>());
+		}
+		
+		if (collision.gameObject.tag == "Fracture")
+		{
+			Physics.IgnoreCollision(GetComponent<MeshCollider>(), collision.gameObject.GetComponent<MeshCollider>());
+		}
+		
+		if (collision.gameObject.tag == "Player")
+		{
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<BoxCollider>(), GetComponent<MeshCollider>(), true);
+		}
+    }
+    
+    void OnCollisionStay(Collision collision)
+    {   
+        
+		if (collision.gameObject.tag == "Bat" || collision.gameObject.tag == "Spider")
+		{
+			Physics.IgnoreCollision(GetComponent<MeshCollider>(), collision.gameObject.GetComponent<CapsuleCollider>());
+		}
+		
+		if (collision.gameObject.tag == "Fracture")
+		{
+			Physics.IgnoreCollision(GetComponent<MeshCollider>(), collision.gameObject.GetComponent<MeshCollider>());
+		}
+		
+		if (collision.gameObject.tag == "Player")
+		{
+			Physics.IgnoreCollision(collision.gameObject.GetComponent<BoxCollider>(), GetComponent<MeshCollider>(), true);
+		}
+    }
+}
