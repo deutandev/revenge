7fc0afb3229f27147fcf1c62436933ffd649e6ae
RedHunter7 <adlirafif42121@gmail.com>
2022-01-07 04:32:59 +0700

    Add Cutscene

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cutscene : MonoBehaviour
+{
+	[System.Serializable]
+	public struct Plot
+	{
+		public Sprite avatar;
+		public string name;
+		[TextArea(5, 10)]
+		public string dialogue;
+	}
+	
+	public Plot[] plot;
+	
+	public Text dialogueText, charaName;
+	public Image avatar;
+	
+	private int index = 0;
+	
+	[HideInInspector]
+	public bool end = false; 
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        string x = plot[index].dialogue;
+		StartCoroutine(ShowText(x));
+    }
+
+    // Update is called once per frame
+    public void ShowPlot() 
+    {
+		index++;
+		if (index == plot.Length) end = true;
+		else if(end == false)
+		{
+			Debug.Log(index);
+			string x = plot[index].dialogue;
+			StartCoroutine(ShowText(x));
+			
+			if(plot[index].name != "0")
+			{
+				charaName.text = plot[index].name;
+				avatar.sprite = plot[index].avatar;	
+			}
+		
+			if(index == plot.Length) end = true;
+		}
+	}
+    
+    IEnumerator ShowText(string text)
+	{
+		dialogueText.text = "";
+		foreach (char c in text) 
+		{
+			dialogueText.text += c;
+			yield return new WaitForSeconds (0.01f);
+		}
+	}
+}
---
+++ Assets/Scripts/CutsceneManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneManager : MonoBehaviour
+{
+	public Cutscene[] cutscene;
+	public Animator fade;
+	private int index = 0;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void StartChangeCutscene() 
+    {
+		StartCoroutine(ChangeCutscene());
+	}
+    
+    IEnumerator ChangeCutscene()
+    {
+		if (cutscene[index].end == true)
+		{
+			index++;
+			fade.SetTrigger("fadeIn");
+			
+			yield return new WaitForSeconds(0.25f);
+			
+			cutscene[index].gameObject.SetActive(true);
+			
+			yield return new WaitForSeconds(0.2f);
+			
+			fade.SetTrigger("fadeOut");
+		}
+	}
+}
